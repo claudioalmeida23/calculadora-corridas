4d35dcb795a043eccbfcbf0ac814f5b8f861b183
Claudio Almeida <59423511+claudioalmeida23@users.noreply.github.com>
2026-03-25 08:21:39 -0300

    Update sw.js
    Signed-off-by: Claudio Almeida <59423511+claudioalmeida23@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,18 +1,40 @@
-const CACHE = "taxi-app-v1";
+const CACHE = "nocaminhocerto-v1";
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
+
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html"
+        "./",
+        "./index.html",
+        "./manifest.json"
       ]);
     })
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    }).catch(() => {
+      return caches.match("./index.html");
+    })
   );
 });
